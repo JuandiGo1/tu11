fc75da7c94d6aafc55a1a5347458f5b6437f0811
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2025-04-22 16:40:10 -0500

    feat: add function to obtain mk value pl

--- server/controllers/jugadores.controller.js
+++ server/controllers/jugadores.controller.js
@@ -1,4 +1,3 @@
-
 import axios from 'axios';
 
 export const buscarJugadores = async (req, res) => {
@@ -6,15 +5,38 @@ export const buscarJugadores = async (req, res) => {
   if (!nombre) return res.status(400).json({ error: 'Falta el nombre a buscar' });
 
   try {
-    const response = await axios.get(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${encodeURIComponent(nombre)}`);
-    const jugadores = response.data.player || [];
+    const response = await axios.get(`https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${encodeURIComponent(nombre)}`);
+    const jugadores = response.data.players || [];
 
     //mostrar solo jugadores de fútbol
     const jugadoresFiltrados = jugadores.filter(j => j.strSport === 'Soccer');
+
 
     res.json(jugadoresFiltrados);
   } catch (error) {
     console.error('Error buscando jugadores:', error.message);
     res.status(500).json({ error: 'Error al buscar jugadores' });
+  }
+};
+
+// Buscar valor de mercado en Transfermarkt
+const obtenerValorMercado = async (idJugador) => {
+  try {
+    // Primera petición: obtener el idTransferMkt desde TheSportsDB
+    const response = await axios.get(`https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${idJugador}`);
+    const idTFMK = response.data.players[0]?.idTransferMkt; // id de Transfermarkt
+
+    if (!idTFMK) return { error: 'No se encontró el id de Transfermarkt' };
+
+    // Segunda petición: obtener el valor de mercado desde Transfermarkt API
+    const marketValueResponse = await axios.get(`https://transfermarkt-api.fly.dev/players/${idTFMK}/market_value`);
+    const marketValue = marketValueResponse.data?.marketValue;
+
+    if (!marketValue) return { error: 'No se encontró el valor de mercado' };
+
+    return { marketValue };
+  } catch (error) {
+    console.error('Error al obtener el valor de mercado:', error.message);
+    return { error: 'Error al obtener el valor de mercado' };
   }
 };
