02930c6872ac200771e6795747280a70fdeaefea
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2025-04-24 10:06:03 -0500

    feat: add Teams and FutPlayers schemas

---
+++ client/lib/schemas.ts
@@ -0,0 +1,34 @@
+
+export interface Regla {
+    formacionFija: boolean; // Si es true, se usará la formación definida en "formacionDefinida"
+    formacionDefinida: '4-4-2' | '4-3-3' | '3-5-2' | '5-3-2' | '4-2-3-1' | null; // Formaciones permitidas
+    permitirJugadoresRepetidos: boolean; // Permitir jugadores repetidos en los equipos
+    ligasPermitidas: string[]; // Lista de ligas permitidas (vacío significa todas las ligas)
+    presupuesto: number | null; // Presupuesto permitido
+}
+
+export interface Sala {
+    codigo: string;
+    reglas : Regla[];
+    jugadores: (link: string) => void;
+    turnoActual: (index: number) => void;
+    
+}
+
+export interface Jugador{
+    nick: string;
+    avatar: string;
+
+}
+
+export interface Futbolista {
+    nombre: string; 
+    posicion: string;
+    valor: number; 
+}
+
+export interface Equipo {
+    nombre: string; // Nombre del equipo
+    futbolistas: Futbolista[]; // Array de futbolistas (máximo 11)
+    presupuestoRestante: number | null; // Presupuesto restante del equipo
+}
--- server/models/JugadorSala.js
+++ server/models/JugadorSala.js
@@ -1,19 +1,20 @@
 import mongoose from 'mongoose';
 
+const FutbolistaSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  posicion: { type: String, required: true }, 
+  valor: { type: Number, required: true } // Valor del futbolista
+});
+
+
 const JugadorSchema = new mongoose.Schema({
   nickname: { type: String, required: true },
   avatar: { type: String },
-  equipo: [
-    {
-      nombre: String,
-      valor: Number,
-      posicion: String,
-      imagen: String,
-    }
-  ],
+  equipo: { type: [FutbolistaSchema], default: []}, // Array de futbolistas},
   presupuestoRestante: { type: Number, default: null }, // Presupuesto restante del jugador (null si no hay límite)
   esCreador: { type: Boolean, default: false }
 });
+
 
 
 export default JugadorSchema;
